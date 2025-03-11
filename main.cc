d408b1a912bb2572a562438a86ac9aa5998b20a3
60b36t <johnmaxrin@gmail.com>
2025-03-11 13:04:10 +0530

    Add OMP to LLVM to Binary Codes

--- main.cc
+++ main.cc
@@ -4,75 +4,100 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Verifier.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/OpenMP/OpenMPDialect.h"
 
 // Conversions
 
 #include "mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
+#include "mlir/Conversion/OpenMPToLLVM/ConvertOpenMPToLLVM.h"
+
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Pass/Pass.h"
 
 using namespace mlir;
 
-int main() {
+int main()
+{
   // Create an MLIR context
   MLIRContext context;
 
   // Load the required dialects
-  context.getOrLoadDialect<arith::ArithDialect>();
-  context.getOrLoadDialect<func::FuncDialect>();
+  context.getOrLoadDialect<mlir::omp::OpenMPDialect>();
+  context.getOrLoadDialect<mlir::LLVM::LLVMDialect>();
 
-  // Create a module
-  OwningOpRef<ModuleOp> module = ModuleOp::create(UnknownLoc::get(&context));
+  mlir::OpBuilder builder(&context);
+  mlir::ModuleOp module = builder.create<mlir::ModuleOp>(builder.getUnknownLoc());
 
-  // Create a builder to help with constructing operations
-  OpBuilder builder(&context);
+  auto ukwnloc = builder.getUnknownLoc();
 
-  // Define the function type (takes no arguments, returns i32)
-  auto funcType = builder.getFunctionType({}, builder.getI32Type());
+  auto i8Type = builder.getI8Type();
+  auto i8PtrType = mlir::LLVM::LLVMPointerType::get(&context);
 
-  // Create a function named "main"
-  auto funcOp = builder.create<func::FuncOp>(
-      builder.getUnknownLoc(), "main", funcType);
+  const char *str = "Hello World\n\0";
+  size_t len = strlen(str);
 
-  // Add the function to the module
-  module->push_back(funcOp);
+  auto arrayType = mlir::LLVM::LLVMArrayType::get(i8Type, len);
 
-  // Create a new block inside the function
-  Block *entryBlock = funcOp.addEntryBlock();
+  auto globalStr = builder.create<mlir::LLVM::GlobalOp>(ukwnloc, arrayType, true, mlir::LLVM::Linkage::Internal, ".str", builder.getStringAttr(str));
 
-  // Set the insertion point to the start of the block
-  builder.setInsertionPointToStart(entryBlock);
+  module.push_back(globalStr);
 
-  // Create a constant value of 42
-  auto constOp = builder.create<arith::ConstantOp>(
-      builder.getUnknownLoc(), builder.getI32Type(),
-      builder.getI32IntegerAttr(42));
+  // Define Printf
+  auto printfType = mlir::LLVM::LLVMFunctionType::get(i8PtrType, {i8PtrType}, true);
+  auto printfFunc = builder.create<mlir::LLVM::LLVMFuncOp>(ukwnloc, "printf", printfType);
 
-  // Return the constant value
-  builder.create<func::ReturnOp>(builder.getUnknownLoc(), constOp.getResult());
+  module.push_back(printfFunc);
 
-  if(failed(verify(module.get())))
-    {
-        llvm::errs()<<"Error: Verification Failed\n";
-        return 0;
-    }
+  auto int32Ty = builder.getI32Type();
 
+  auto funcType = LLVM::LLVMFunctionType::get(int32Ty, {int32Ty});
 
+  auto funcOp = builder.create<mlir::LLVM::LLVMFuncOp>(ukwnloc, "main", funcType);
+  module.push_back(funcOp);
 
-  // Print the MLIR code to stdout
-  module->dump();
+  auto block = funcOp.addEntryBlock(builder);
 
+  builder.setInsertionPointToStart(block);
 
-  // All about Pass
-  PassManager pm(&context);
-  pm.addPass(createArithToLLVMConversionPass());
+  auto zero = builder.create<mlir::LLVM::ConstantOp>(ukwnloc, int32Ty, builder.getI32IntegerAttr(0));
+  auto one = builder.create<mlir::LLVM::ConstantOp>(ukwnloc, int32Ty, builder.getI32IntegerAttr(1));
+  auto strPtr = builder.create<mlir::LLVM::AddressOfOp>(ukwnloc, globalStr);
 
-  if (mlir::failed(pm.run(*module))) {
-    llvm::errs() << "Failed to convert to LLVM dialect\n";
-    return 1;
+  auto gep = builder.create<mlir::LLVM::GEPOp>(ukwnloc, i8PtrType, arrayType, strPtr, mlir::ValueRange{zero, zero});
+
+ 
+  mlir::Location dummyLoc = mlir::FileLineColLoc::get(builder.getStringAttr("dummy.mlir"), 0, 0);
+  auto parallelOp = builder.create<mlir::omp::ParallelOp>(dummyLoc);
+  auto &parallelRegion = parallelOp.getRegion();
+  auto *parallelBlock = builder.createBlock(&parallelRegion);
+  builder.setInsertionPointToStart(parallelBlock);
+
+  // USE GEP
+  // auto gep = builder.create<mlir::LLVM::GEPOp>(ukwnloc, i8PtrType, arrayType, strPtr, mlir::ValueRange{zero,zero});
+  builder.create<mlir::LLVM::CallOp>(ukwnloc, printfFunc, mlir::ValueRange{gep});
+  builder.create<mlir::omp::TerminatorOp>(ukwnloc);
+  builder.setInsertionPointAfter(parallelOp);
+
+  // Return 33
+  // Creare 33
+  auto constOp = builder.create<mlir::LLVM::ConstantOp>(ukwnloc, int32Ty, builder.getI32IntegerAttr(33));
+  auto retOp = builder.create<mlir::LLVM::ReturnOp>(ukwnloc, constOp->getResult(0));
+
+  if (failed(verify(module)))
+  {
+    llvm::errs() << "Error: Verification Failed\n";
+    return 0;
   }
 
-
+  PassManager pm(&context);
+  pm.addPass(createConvertOpenMPToLLVMPass());
+  if (failed(pm.run(module)))
+  {
+    llvm::errs() << "Failed to run passes\n";
+    return 1;
+  }
+  // Print the MLIR code to stdout
   module->dump();
 
   return 0;
